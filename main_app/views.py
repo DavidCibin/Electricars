2f8617fb3b95a19e029f6e1056af3eadcaba5211
davidcibin <davidisurf@hotmail.com>
2021-05-06 01:43:13 -0400

    basic home and about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
