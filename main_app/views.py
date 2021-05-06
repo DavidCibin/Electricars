ab2fdd3d1091c088970877daa3ce78518bcafdf2
davidcibin <davidisurf@hotmail.com>
2021-05-06 00:14:24 -0400

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
