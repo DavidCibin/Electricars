687db227bef3609f6ffc5cea93ca58bbd87066e2
David Cibin <ddcibin@gmail.com>
2023-10-02 14:09:09 -0400

    update some setup stuff v.5

--- electricars/settings.py
+++ electricars/settings.py
@@ -30,22 +30,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
-MAILCHIMP_API_KEY= env('MAILCHIMP_API_KEY')
-MAILCHIMP_DATA_CENTER= env('MAILCHIMP_DATA_CENTER')
-MAILCHIMP_EMAIL_LIST_ID= env('MAILCHIMP_EMAIL_LIST_ID')
-AWS_ACCESS_KEY_ID= env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY= env('AWS_SECRET_ACCESS_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
+MAILCHIMP_API_KEY= os.getenv('MAILCHIMP_API_KEY')
+MAILCHIMP_DATA_CENTER= os.getenv('MAILCHIMP_DATA_CENTER')
+MAILCHIMP_EMAIL_LIST_ID= os.getenv('MAILCHIMP_EMAIL_LIST_ID')
+AWS_ACCESS_KEY_ID= os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY= os.getenv('AWS_SECRET_ACCESS_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in env
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = env('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -102,7 +102,7 @@ DATABASES = {
 	# 	'ENGINE': 'django.db.backends.postgresql',
 	# 	'NAME': 'electricars',
 	# }
-    "default": dj_database_url.parse(env("DATABASE_URL"))
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 
